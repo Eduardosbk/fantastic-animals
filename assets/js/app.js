f7f116f031db775216d524fcfa0ca41d1348ad47
Eduardo Candido <eduardo_sbk589@hotmail.com>
2021-10-23 04:17:08 -0400

    Moving animation scroll position

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 import SmoothScroll from './modules/smooth-scroll.js';
 import initTabNav from './modules/tabnav.js';
 import initAccordion from './modules/accordion.js';
-import initAnimationScroll from './modules/animation-scroll.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown.js';
@@ -9,12 +8,13 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initOperation from './modules/operation.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import initAnimationScroll from './modules/animation-scroll.js';
 
 const smoothscroll = new SmoothScroll('[data-menu="smooth"]');
 smoothscroll.init();
+
 initTabNav();
 initAccordion();
-initAnimationScroll();
 initModal();
 initTooltip();
 initDropdownMenu();
@@ -22,3 +22,4 @@ initMenuMobile();
 initOperation();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimationScroll();
