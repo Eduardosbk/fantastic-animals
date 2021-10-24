f9c9e9584d3e8680e500b36b257c18b3461c9fc5
Eduardo Candido <eduardo_sbk589@hotmail.com>
2021-10-24 18:47:55 -0400

    operation function refactored to class

--- assets/js/modules/operation.js
+++ assets/js/modules/operation.js
@@ -1,13 +1,37 @@
-export default function initOperation() {
-  const operation = document.querySelector('[data-week]');
-  const weekDays = operation.dataset.week.split(',').map(Number);
-  const scheduleWeek = operation.dataset.hour.split(',').map(Number);
-  const dateNow = new Date();
-  const dayWeek = dateNow.getDay();
-  const hourNow = dateNow.getHours();
-  const weekOpen = weekDays.indexOf(dayWeek) !== -1;
-  const scheduleOpen = (hourNow >= scheduleWeek[0] && hourNow < scheduleWeek[1]);
-  if (weekOpen && scheduleOpen) {
-    operation.classList.add('open');
+export default class Operation {
+  constructor(operation, activeClass) {
+    this.operation = document.querySelector(operation);
+    this.activeClass = activeClass;
+  }
+
+  operationData() {
+    this.weekDays = this.operation.dataset.week.split(',').map(Number);
+    this.scheduleWeek = this.operation.dataset.hour.split(',').map(Number);
+  }
+  
+  dataNow() {
+    this.dateNow = new Date();
+    this.dayNow = this.dateNow.getDay();
+    this.hourNow = this.dateNow.getUTCHours() - 3;
+  }
+  
+  isOpen() {
+    const weekOpen = this.weekDays.indexOf(this.dayNow) !== -1;
+    const scheduleOpen = (this.hourNow >= this.scheduleWeek[0] && this.hourNow < this.scheduleWeek[1]);
+    return weekOpen && scheduleOpen;
+  }
+
+  activeOpen() {
+    if (this.isOpen()) {
+      this.operation.classList.add(this.activeClass);
+    }
+  }
+  init() {
+    if(this.operation) {
+      this.operationData();
+      this.dataNow();
+      this.activeOpen();
+    }
+    return this;
   }
 };
